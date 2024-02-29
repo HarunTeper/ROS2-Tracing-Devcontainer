93a39ba59ab42304f87ebc7ac70e8014c1835a79
HarunTeper <hteper@web.de>
2024-02-29 12:09:32 +0100

    added devcontainer and setup script

---
+++ tracing_setup.sh
@@ -0,0 +1,9 @@
+cd /home/vscode/workspace
+mkdir -p ros2_tracing_ws/src
+git clone -b humble https://gitlab.com/ros-tracing/ros2_tracing.git /home/vscode/workspace/ros2_tracing_ws/src/
+cd /home/vscode/workspace/ros2_tracing_ws
+
+. /opt/ros/humble/setup.sh
+colcon build --symlink-install --allow-overriding tracetools
+
+echo 'source /home/vscode/workspace/ros2_tracing_ws/install/setup.bash' >> /home/vscode/.bashrc
